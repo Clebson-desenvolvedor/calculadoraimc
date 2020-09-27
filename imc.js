d4c2f721d5be077560138d9b54ba769a28245e37
Clebson Araújo <clebson.araujo.25@gmail.com>
2020-09-26 21:57:42 -0300

    primeiro commit

---
+++ imc.js
@@ -0,0 +1,20 @@
+function calcular(peso, altura){
+    var peso = document.getElementById('txtPeso').value;
+    var altura = document.getElementById('txtAltura').value;
+    var imc = peso / (altura * altura);
+    document.getElementById('resultado').innerHTML = imc.toFixed(2);
+
+    if(imc < 18.5){
+        document.getElementById('magreza').classList.add('destaque');
+    } else if(imc >= 18.5 && imc <= 24.9){
+        document.getElementById('normal').classList.add('destaque');
+    } else if(imc >= 25 && imc <= 29.9){
+        document.getElementById('sobrepeso').classList.add('destaque');
+    } else if(imc >= 30 && imc <= 39.9){
+        document.getElementById('obesidade').classList.add('destaque');
+    } else if(imc > 40){
+        document.getElementById('obesidadegrave').classList.add('destaque');
+    } else{
+        document.getElementById('resultado').innerHTML = "digite um valor válido"
+    }
+}
