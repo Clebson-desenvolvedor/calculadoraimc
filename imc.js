c3809a9992f919c00514472e95dbf451a4c6a4c9
Clebson Araújo <44328963+Clebson-desenvolvedor@users.noreply.github.com>
2021-01-01 14:54:13 -0300

    Update imc.js
    as linhas condicionais foram modificadas para ser usada menos linhas de código.

--- imc.js
+++ imc.js
@@ -3,18 +3,18 @@ function calcular(peso, altura){
     var altura = document.getElementById('txtAltura').value;
     var imc = peso / (altura * altura);
     document.getElementById('resultado').innerHTML = imc.toFixed(2);
-
+    
     if(imc < 18.5){
         document.getElementById('magreza').classList.add('destaque');
-    } else if(imc >= 18.5 && imc <= 24.9){
+    } else if(imc <= 24.9){
         document.getElementById('normal').classList.add('destaque');
-    } else if(imc >= 25 && imc <= 29.9){
+    } else if(imc <= 29.9){
         document.getElementById('sobrepeso').classList.add('destaque');
-    } else if(imc >= 30 && imc <= 39.9){
+    } else if(imc <= 39.9){
         document.getElementById('obesidade').classList.add('destaque');
     } else if(imc > 40){
         document.getElementById('obesidadegrave').classList.add('destaque');
     } else{
-        document.getElementById('resultado').innerHTML = "digite um valor válido"
+        document.getElementById('resultado').innerHTML = 'digite um valor válido';
     }
 }
