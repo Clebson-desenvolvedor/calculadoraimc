31a54e5171d6f1f2c0625073c8f75c4d2964f80e
clebson.araujo <clebson.araujo@l5.com.br>
2025-03-06 22:20:50 -0300

    Feat - organização estrutura e criado arquivo ts

--- dist/imc.js
+++ dist/imc.js
@@ -1,20 +1,36 @@
-function calcular(peso, altura){
-    var peso = document.getElementById('txtPeso').value;
-    var altura = document.getElementById('txtAltura').value;
-    var imc = peso / (altura * altura);
-    document.getElementById('resultado').innerHTML = imc.toFixed(2);
-    
-    if(imc < 18.5){
-        document.getElementById('magreza').classList.add('destaque');
-    } else if(imc <= 24.9){
-        document.getElementById('normal').classList.add('destaque');
-    } else if(imc <= 29.9){
-        document.getElementById('sobrepeso').classList.add('destaque');
-    } else if(imc <= 39.9){
-        document.getElementById('obesidade').classList.add('destaque');
-    } else if(imc > 40){
-        document.getElementById('obesidadegrave').classList.add('destaque');
-    } else{
-        document.getElementById('resultado').innerHTML = 'digite um valor válido';
+"use strict";
+function calcular() {
+    let peso = document.getElementById("txtPeso");
+    let altura = document.getElementById("txtAltura");
+    let imc = 0;
+    if (peso && altura) {
+        imc = parseFloat(peso.value) / (parseFloat(altura.value) * parseFloat(altura.value));
+    }
+    else {
+        throw new Error("possíveis problema nas variáveis peso ou altura");
+    }
+    let resultado = document.getElementById("resultado");
+    if (resultado) {
+        resultado.innerHTML = imc.toFixed(2);
+    }
+    console.log(imc);
+    const estado_imc = {
+        magreza: imc < 18.5,
+        normal: imc >= 18.5 && imc <= 24.9,
+        sobrepeso: imc >= 25 && imc <= 29.9,
+        obesidade: imc >= 30 && imc <= 39.9,
+        obesidadegrave: imc >= 40
+    };
+    for (const estado in estado_imc) {
+        const elemento = document.getElementById(estado);
+        if (elemento) {
+            if (estado_imc[estado]) {
+                elemento.classList.add("destaque");
+            }
+            else {
+                elemento.classList.remove("destaque");
+            }
+        }
     }
 }
+window.calcular = calcular;
--- src/imc.ts
+++ src/imc.ts
@@ -1,20 +1,41 @@
-function calcular() {
-    let peso = document.getElementById('txtPeso').value
-    let altura = document.getElementById('txtAltura').value;
-    let imc = peso / (altura * altura);
-    document.getElementById('resultado').innerHTML = imc.toFixed(2);
+function calcular(): void {
+    let peso = document.getElementById("txtPeso") as HTMLInputElement | null;
+    let altura = document.getElementById("txtAltura") as HTMLInputElement | null;
+
+    let imc: number = 0;
+
+    if (peso && altura) {
+        imc = parseFloat(peso.value) / (parseFloat(altura.value) * parseFloat(altura.value));
+    } else {
+        throw new Error("possíveis problema nas variáveis peso ou altura");
+    }
+
+    let resultado = document.getElementById("resultado");
+
+    if (resultado) {
+        resultado.innerHTML = imc.toFixed(2);
+    }
+
+    console.log(imc)
+
+    const estado_imc: object = {
+        magreza: imc < 18.5,
+        normal: imc >= 18.5 && imc <= 24.9,
+        sobrepeso: imc >= 25 && imc <= 29.9,
+        obesidade: imc >= 30 && imc <= 39.9,
+        obesidadegrave: imc >= 40
+    };
     
-    if(imc < 18.5){
-        document.getElementById('magreza').classList.add('destaque');
-    } else if(imc <= 24.9){
-        document.getElementById('normal').classList.add('destaque');
-    } else if(imc <= 29.9){
-        document.getElementById('sobrepeso').classList.add('destaque');
-    } else if(imc <= 39.9){
-        document.getElementById('obesidade').classList.add('destaque');
-    } else if(imc > 40){
-        document.getElementById('obesidadegrave').classList.add('destaque');
-    } else{
-        document.getElementById('resultado').innerHTML = 'digite um valor válido';
+    for (const estado in estado_imc) {
+        const elemento = document.getElementById(estado);
+        if (elemento) {
+            if (estado_imc[estado as keyof typeof estado_imc]) {
+                elemento.classList.add("destaque");
+            } else {
+                elemento.classList.remove("destaque");
+            }
+        }
     }
 }
+
+window.calcular = calcular;
